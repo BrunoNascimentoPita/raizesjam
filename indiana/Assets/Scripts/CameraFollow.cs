701dcf48fa7980f90646f0f8d4f9b8a398dd61f4
Bruno Nascimento <57575705+BrunoNascimentoPita@users.noreply.github.com>
2025-02-23 07:35:45 -0300

    Cinemachine no pixel perfect camera

--- indiana/Assets/Scripts/CameraFollow.cs
+++ indiana/Assets/Scripts/CameraFollow.cs
@@ -2,22 +2,20 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-
     public Transform target;
     public float smoothSpeed;
     public Vector3 offset;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
 
+        // Aplica suavização com Lerp
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        // Arredonda para evitar tremedeira
+        smoothedPosition.x = Mathf.Round(smoothedPosition.x * 100) / 100;
+        smoothedPosition.y = Mathf.Round(smoothedPosition.y * 100) / 100;
 
         transform.position = smoothedPosition;
     }
